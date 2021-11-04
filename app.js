bf0bec4ff5458641cee7275b3ba648a1df177b7e
unknown <hdmoviez1234@gmail.com>
2021-11-04 12:33:11 -0800

    commit

---
+++ app.js
@@ -0,0 +1,46 @@
+//              /////   ARRAYS    /////
+// var students = ["usama","talha","taha",3,5];
+// // students.push("huzaifa","bilal",1,5);
+// // students.pop();
+// // students.shift();
+// students.unshift("huzaifa","bilal",1,5);
+// console.log(students);  
+
+
+
+
+
+
+
+
+// var array = [];
+// array[0] = 2060 ;
+// array[1] = 1012 ;
+// array[2] = 2101 ;
+// console.log(array);
+
+
+
+// for(i=1; i<=10; i++){
+//     document.write(i + "<br />")
+// }
+
+
+
+// var tnum = prompt("Enter Table Number");
+// var tlength = prompt("Enter Table Length");
+
+// // for(i=1; i<=tlength; i++){
+// //     document.write(tnum + "x" + i + "=" + tnum*i +"<br />")
+// // }
+
+
+
+// var qualifaction = ["SSC", "HSC", "BCS","BS", "BCOM", "MS", "M", "Phil", "PhD"];
+// document.write(qualifaction); 
+
+
+// var arr = ["This", "is", "my", "cat"];
+// document.write(arr);
+// var arr = ["<br />" + "This is my cat"];
+// document.write(arr);
